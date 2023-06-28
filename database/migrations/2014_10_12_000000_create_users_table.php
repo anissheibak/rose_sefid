07b978f89c74f8b2afe6b5f5b4b807a1ee7b4d8a
anissheibak <sheibakanis@gmail.com>
2023-06-29 03:31:16 +0430

    add some migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,12 +13,23 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('email')->unique()->nullable();
+            $table->string('mobile')->unique()->nullable();
+            $table->string('password')->nullable();
+            $table->string('national_code')->unique()->nullable();
+            $table->string('first_name')->nullable();
+            $table->string('last_name')->nullable();
+            $table->string('slug')->unique()->nullable();
+            $table->text('profile_photo_path')->nullable()->comment('avatar');
+            $table->timestamp('email_verifed_at')->nullable();
+            $table->tinyInteger('activation')->default(0)->comment('0=> inactive, 1 => active');
+            $table->tinyInteger('activation_date')->nullable();
+            $table->tinyInteger('user_type')->default(0)->comment('0 => user, 1 => admin');
+            $table->tinyInteger('status')->default(0);
+            $table->foreignId('current_team_id')->nullable();
             $table->rememberToken();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
