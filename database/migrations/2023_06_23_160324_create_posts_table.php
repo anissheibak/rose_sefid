14c6bcd56ddfd690a4ae22d308abbeea8bfcade3
anissheibak <sheibakanis@gmail.com>
2023-06-29 22:24:40 +0430

    faqs, menus migrations addes

--- database/migrations/2023_06_23_160324_create_posts_table.php
+++ database/migrations/2023_06_23_160324_create_posts_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
-
             $table->string('name');
             $table->string('slug')->unique()->nullable();
             $table->text('summary');
