aa465711c1a8fee8cd880f4b7ba25cb66a396d7d
anissheibak <sheibakanis@gmail.com>
2023-06-29 22:08:15 +0430

    post categories table

--- database/migrations/2023_06_23_160324_creat_posts_table.php
+++ database/migrations/2023_06_23_160324_creat_posts_table.php
@@ -13,7 +13,20 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+
+            $table->string('name');
+            $table->string('slug')->unique()->nullable();
+            $table->text('summary');
+            $table->text('body');
+            $table->text('image');
+            $table->tinyInteger('status')->default(0);
+            $table->tinyInteger('commantable')->default(0)->comment('0 => uncommentable, 1 => commentable');
+            $table->string('tags');
+            $table->timestamp('published_at');
+            $table->foreignId('author_id')->constrained('users');
+            $table->foreignId('category_id')->constrained('post_categories');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
