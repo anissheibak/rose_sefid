8c29a4d287de6ff0ed6469ac5ab7f8e098270881
anissheibak <sheibakanis@gmail.com>
2023-07-03 20:25:19 +0430

    add ticket's migrations

---
+++ database/migrations/2023_06_29_180750_create_comments_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->text('body');
+            $table->foreignId('parent_id')->constrained('comments');
+            $table->foreignId('author_id')->constrained('users');
+            $table->unsignedBigInteger('commentable_id');
+            $table->string('commentable_type');
+            $table->tinyInteger('seen')->default(0)->comment('0 => seen, 1 => unseen');
+            $table->tinyInteger('approved')->default(0)->comment('0 => not approved, 1 => approved');
+            $table->tinyInteger('status')->default(0);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comments');
+    }
+};
