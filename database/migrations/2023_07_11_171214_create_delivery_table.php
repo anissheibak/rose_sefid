a50393b0ebcffb29e0e4ace0b19e92589ba79105
anissheibak <sheibakanis@gmail.com>
2023-07-11 21:55:59 +0430

    add addresses migrations

---
+++ database/migrations/2023_07_11_171214_create_delivery_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('delivery', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->decimal('amount', 20, 3)->nullable()->comment('if it was null means the sending is free');
+            $table->integer('delivery_time')->nullable();
+            $table->string('delivery_time_unit')->nullable();
+            $table->tinyInteger('status')->default(0);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('delivery');
+    }
+};
