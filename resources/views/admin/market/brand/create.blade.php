93bfb776b4bf63675f4550d46301345acc256230
anissheibak <sheibakanis@gmail.com>
2023-07-13 00:00:13 +0430

    Add admin-panel folders

---
+++ resources/views/admin/market/brand/create.blade.php
@@ -0,0 +1,60 @@
+@extends('admin.layouts.master')
+
+@section('head-tag')
+<title>برند</title>
+@endsection
+
+@section('content')
+
+<nav aria-label="breadcrumb">
+    <ol class="breadcrumb">
+      <li class="breadcrumb-item font-size-12"> <a href="#">خانه</a></li>
+      <li class="breadcrumb-item font-size-12"> <a href="#">بخش فروش</a></li>
+      <li class="breadcrumb-item font-size-12"> <a href="#">برند</a></li>
+      <li class="breadcrumb-item font-size-12 active" aria-current="page"> ایجاد برند</li>
+    </ol>
+  </nav>
+
+
+  <section class="row">
+    <section class="col-12">
+        <section class="main-body-container">
+            <section class="main-body-container-header">
+                <h5>
+                  ایجاد برند
+                </h5>
+            </section>
+
+            <section class="d-flex justify-content-between align-items-center mt-4 mb-3 border-bottom pb-2">
+                <a href="{{ route('admin.market.brand.index') }}" class="btn btn-info btn-sm">بازگشت</a>
+            </section>
+
+            <section>
+                <form action="" method="">
+                    <section class="row">
+
+                        <section class="col-12 col-md-6">
+                            <div class="form-group">
+                                <label for="">نام برند</label>
+                                <input type="text" class="form-control form-control-sm">
+                            </div>
+                        </section>
+
+                        <section class="col-12 col-md-6">
+                            <div class="form-group">
+                                <label for="">تصویر برند</label>
+                                <input type="file" class="form-control form-control-sm">
+                            </div>
+                        </section>
+                        <section class="col-12">
+                            <button class="btn btn-primary btn-sm">ثبت</button>
+                        </section>
+                    </section>
+                </form>
+            </section>
+
+        </section>
+    </section>
+</section>
+
+@endsection
