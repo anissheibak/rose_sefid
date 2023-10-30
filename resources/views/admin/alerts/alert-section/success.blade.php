9ded98125311e0df7018c53a6090cfd1acc20dfc
anissheibak <sheibakanis@gmail.com>
2023-10-30 22:13:30 +0330

    alerts added.

---
+++ resources/views/admin/alerts/alert-section/success.blade.php
@@ -0,0 +1,14 @@
+@if (session('alert-section-success'))
+
+    <div class="alert alert-success alert-dismissible fade show" role="alert">
+        <h4 class="alert-heading">&times; خطا</h4>
+        <hr>
+        <p class="mb-0">
+            {{session('alert-section-success')}}
+        </p>
+
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close" style="right: auto !important; left: 0 !important;">
+            <span aria-hidden="true">&times;</span>
+        </button>
+    </div>
+@endif
