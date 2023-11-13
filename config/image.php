a7d0bbf1e53d03852be9fb72006480e73173affb
anissheibak <sheibakanis@gmail.com>
2023-11-13 17:52:58 +0330

    Image Cache Service implemented.

--- config/image.php
+++ config/image.php
@@ -33,6 +33,26 @@ return [
         ]
     ],
 
-    'default-current-index-image'=> 'medium'
+    'default-current-index-image'=> 'medium',
+
+    //index size
+    'cache-image-sizes' => [
+        'large' => [
+            'width' => 800,
+            'height'=> 600
+        ],
+        'medium'=> [
+            'width'=> 400,
+            'height'=> 300
+        ],
+        'small'=> [
+            'width'=> 80,
+            'height'=> 60
+        ]
+    ],
+
+    'default-current-cache-image' => 'medium',
+    'image-cache-life-time' => 10,
+    'image-not-found' => ''
 
 ];
