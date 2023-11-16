cf4039e8e435c42cc95d97b011b5dcc6adab224c
anissheibak <sheibakanis@gmail.com>
2023-11-16 23:59:28 +0330

    Posts completed

--- resources/views/admin/content/category/edit.blade.php
+++ resources/views/admin/content/category/edit.blade.php
@@ -1,7 +1,7 @@
 @extends('admin.layouts.master')
 
 @section('head-tag')
-<title>دسته بندی</title>
+<title>ویرایش دسته بندی</title>
 @endsection
 
 @section('content')
@@ -11,7 +11,7 @@
       <li class="breadcrumb-item font-size-12"> <a href="#">خانه</a></li>
       <li class="breadcrumb-item font-size-12"> <a href="#">بخش فروش</a></li>
       <li class="breadcrumb-item font-size-12"> <a href="#">دسته بندی</a></li>
-      <li class="breadcrumb-item font-size-12 active" aria-current="page"> ایجاد دسته بندی</li>
+      <li class="breadcrumb-item font-size-12 active" aria-current="page"> ویرایش دسته بندی</li>
     </ol>
   </nav>
 
@@ -21,7 +21,7 @@
         <section class="main-body-container">
             <section class="main-body-container-header">
                 <h5>
-                  ایجاد دسته بندی
+                  ویرایش دسته بندی
                 </h5>
             </section>
 
@@ -156,32 +156,32 @@
         CKEDITOR.replace('description');
     </script>
 
-<script>
-    $(document).ready(function(){
-        var tags_input = $('#tags');
-        var select_tags = $('#select_tags');
-        var default_tags = tags_input.val();
-        var default_data = null;
+    <script>
+        $(document).ready(function(){
+            var tags_input = $('#tags');
+            var select_tags = $('#select_tags');
+            var default_tags = tags_input.val();
+            var default_data = null;
 
-        if(tags_input.val() !== null && tags_input.val().length > 0){
-            default_data =default_tags.split(',')
-        }
-
-        select_tags.select2({
-            placeholder : 'لطفا تگ‌های خود را وارد نمایید.',
-            tags : true,
-            data: default_data
-        });
-
-        select_tags.children('option').attr('selected', true).trigger('change');
-
-        $('#form').submit(function(event){
-            if(select_tags.val() !== null && select_tags.val().length > 0){
-                var selectedSource = select_tags.val().join(',');
-                tags_input.val(selectedSource)
+            if(tags_input.val() !== null && tags_input.val().length > 0){
+                default_data =default_tags.split(',')
             }
+
+            select_tags.select2({
+                placeholder : 'لطفا تگ‌های خود را وارد نمایید.',
+                tags : true,
+                data: default_data
+            });
+
+            select_tags.children('option').attr('selected', true).trigger('change');
+
+            $('#form').submit(function(event){
+                if(select_tags.val() !== null && select_tags.val().length > 0){
+                    var selectedSource = select_tags.val().join(',');
+                    tags_input.val(selectedSource)
+                }
+            })
         })
-    })
-</script>
+    </script>
 
 @endsection
