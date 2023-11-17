ad99d767673a6b3d3d57d260fd557036b4fc9b21
anissheibak <sheibakanis@gmail.com>
2023-11-17 22:06:15 +0330

    validation and routs updated

--- routes/web.php
+++ routes/web.php
@@ -213,9 +213,11 @@ Route::prefix('admin')->namespace('Admin')->group(function(){
         Route::get('/', [MenuController::class, 'index'])->name('admin.content.menu.index');
         Route::get('/create', [MenuController::class, 'create'])->name('admin.content.menu.create');
         Route::post('/store', [MenuController::class, 'store'])->name('admin.content.menu.store');
-        Route::get('/edit/{id}', [MenuController::class, 'edit'])->name('admin.content.menu.edit');
-        Route::put('/update/{id}', [MenuController::class, 'update'])->name('admin.content.menu.update');
-        Route::delete('/destroy/{id}', [MenuController::class, 'destroy'])->name('admin.content.menu.destroy');
+        Route::get('/edit/{menu}', [MenuController::class, 'edit'])->name('admin.content.menu.edit');
+        Route::put('/update/{menu}', [MenuController::class, 'update'])->name('admin.content.menu.update');
+        Route::delete('/destroy/{menu}', [MenuController::class, 'destroy'])->name('admin.content.menu.destroy');
+        Route::get('/status/{menu}', [MenuController::class, 'status'])->name('admin.content.menu.status');
+
 });
 
 //page
@@ -223,9 +225,11 @@ Route::prefix('admin')->namespace('Admin')->group(function(){
         Route::get('/', [PageController::class, 'index'])->name('admin.content.page.index');
         Route::get('/create', [PageController::class, 'create'])->name('admin.content.page.create');
         Route::post('/store', [PageController::class, 'store'])->name('admin.content.page.store');
-        Route::get('/edit/{id}', [PageController::class, 'edit'])->name('admin.content.page.edit');
-        Route::put('/update/{id}', [PageController::class, 'update'])->name('admin.content.page.update');
-        Route::delete('/destroy/{id}', [PageController::class, 'destroy'])->name('admin.content.page.destroy');
+        Route::get('/edit/{page}', [PageController::class, 'edit'])->name('admin.content.page.edit');
+        Route::put('/update/{page}', [PageController::class, 'update'])->name('admin.content.page.update');
+        Route::delete('/destroy/{page}', [PageController::class, 'destroy'])->name('admin.content.page.destroy');
+        Route::get('/status/{page}', [PageController::class, 'status'])->name('admin.content.page.status');
+
 });
 
 //post
