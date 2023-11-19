fe15603e1b9619ea0a1dd18828717c647e3364a2
anissheibak <sheibakanis@gmail.com>
2023-11-19 05:16:02 +0330

    site setting migration, seeder, index and model created

---
+++ database/seeders/SettingSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class SettingSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table("settings")->insert([
+            "title"=> "عنوان سایت",
+            "description"=> "توضیحات سایت",
+            "keywords"=> "کلمات کلیدی سایت",
+            "logo"=> "logo.png",
+            "icon"=> "icon.png",
+        ]);
+    }
+}
