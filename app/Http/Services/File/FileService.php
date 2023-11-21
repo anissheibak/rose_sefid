2a873bf65f4d8337257d1fe009c7fe05eb8931f1
anissheibak <sheibakanis@gmail.com>
2023-11-22 02:50:24 +0330

    Email File Service implementedd.

---
+++ app/Http/Services/File/FileService.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Services\File;
+
+class FileService extends FileToolsService
+{
+    public function moveToPublic($file){
+
+        //set file
+        $this->setFile($file);
+
+        //execute provider
+        $this->provider();
+
+        //save file
+        $result = $file->move(public_path($this->getFinalFileDirectory()), $this->getFinalFileName());
+
+        return $result ? $this->getFileAddress() : false;
+    }
+
+    public function moveToStorage($file){
+
+        //set file
+        $this->setFile($file);
+
+        //execute provider
+        $this->provider();
+
+        //save file
+        $result = $file->move(storage_path($this->getFinalFileDirectory()), $this->getFinalFileName());
+
+        return $result ? $this->getFileAddress() : false;
+    }
+
+
+    public function deleteFile($filePath, $storage = false){
+
+        if($storage){
+            unlink(storage_path($filePath));
+            return true;
+        }
+
+        if(file_exists($filePath)){
+            unlink($filePath);
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+}
+
