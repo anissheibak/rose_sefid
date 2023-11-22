b3691c3b1abacbf3524ec570f578976fd2c0894d
anissheibak <sheibakanis@gmail.com>
2023-11-23 02:18:00 +0330

    Admin User create and index added.

--- resources/views/admin/user/admin-user/create.blade.php
+++ resources/views/admin/user/admin-user/create.blade.php
@@ -30,61 +30,131 @@
             </section>
 
             <section>
-                <form action="" method="">
+                <form action="{{ route('admin.user.admin-user.store')}}" method="POST" enctype="multipart/form-data">
+                    @csrf
+
                     <section class="row">
 
-                        <section class="col-12 col-md-6">
+                        <section class="col-12 col-md-6 my-2">
                             <div class="form-group">
-                                <label for="">نام</label>
-                                <input type="text" class="form-control form-control-sm">
+                                <label for="first_name">نام</label>
+                                <input type="text" name="first_name" class="form-control form-control-sm" value="{{old('first_name')}}">
                             </div>
-                        </section>
-                        <section class="col-12 col-md-6">
-                            <div class="form-group">
-                                <label for="">نام خانوادگی</label>
-                                <input type="text" class="form-control form-control-sm">
-                            </div>
-                        </section>
-                        <section class="col-12 col-md-6">
-                            <div class="form-group">
-                                <label for="">ایمیل</label>
-                                <input type="text" class="form-control form-control-sm">
-                            </div>
-                        </section>
-                     <section class="col-12 col-md-6">
-                            <div class="form-group">
-                                <label for=""> شماره موبایل</label>
-                                <input type="text" class="form-control form-control-sm">
-                            </div>
-                        </section>
-                     <section class="col-12 col-md-6">
-                            <div class="form-group">
-                                <label for="">کلمه عبور</label>
-                                <input type="text" class="form-control form-control-sm">
-                            </div>
-                        </section>
-                    <section class="col-12 col-md-6">
-                            <div class="form-group">
-                                <label for="">تکرار کلمه عبور</label>
-                                <input type="text" class="form-control form-control-sm">
-                            </div>
-                        </section>
-                    <section class="col-12 col-md-6">
-                            <div class="form-group">
-                                <label for="">تصویر</label>
-                                <input type="file" class="form-control form-control-sm">
-                            </div>
+                            @error('first_name')
+                                <span class="alert_required bg-danger text-white p-1 rounded" role="alert">
+                                    <strong>
+                                        {{$message}}
+                                    </strong>
+                                </span>
+                            @enderror
                         </section>
 
-                        <section class="col-12 col-md-6">
+
+                        <section class="col-12 col-md-6 my-2">
                             <div class="form-group">
-                                <label for="">وضعیت کاربر</label>
-                                <select name="" id="" class="form-control form-control-sm">
-                                    <option value="">غیر فعال</option>
-                                    <option value="">فعال</option>
+                                <label for="last_name">نام خانوادگی</label>
+                                <input type="text" name="last_name" class="form-control form-control-sm" value="{{old('last_name')}}">
+                            </div>
+                            @error('last_name')
+                                <span class="alert_required bg-danger text-white p-1 rounded" role="alert">
+                                    <strong>
+                                        {{$message}}
+                                    </strong>
+                                </span>
+                            @enderror
+                        </section>
+
+
+                        <section class="col-12 col-md-6 my-2">
+                            <div class="form-group">
+                                <label for="email">ایمیل</label>
+                                <input type="text" name="email" class="form-control form-control-sm" value="{{old('email')}}">
+                            </div>
+                            @error('email')
+                                <span class="alert_required bg-danger text-white p-1 rounded" role="alert">
+                                    <strong>
+                                        {{$message}}
+                                    </strong>
+                                </span>
+                            @enderror
+                        </section>
+
+
+                        <section class="col-12 col-md-6 my-2">
+                            <div class="form-group">
+                                <label for="mobile"> شماره موبایل</label>
+                                <input type="text" name="mobile" class="form-control form-control-sm" value="{{old('mobile')}}">
+                            </div>
+                            @error('mobile')
+                                <span class="alert_required bg-danger text-white p-1 rounded" role="alert">
+                                    <strong>
+                                        {{$message}}
+                                    </strong>
+                                </span>
+                            @enderror
+                        </section>
+
+
+                        <section class="col-12 col-md-6 my-2">
+                            <div class="form-group">
+                                <label for="password">کلمه عبور</label>
+                                <input type="password" name="password" class="form-control form-control-sm">
+                            </div>
+                            @error('password')
+                                <span class="alert_required bg-danger text-white p-1 rounded" role="alert">
+                                    <strong>
+                                        {{$message}}
+                                    </strong>
+                                </span>
+                            @enderror
+                        </section>
+
+                        <section class="col-12 col-md-6 my-2">
+                            <div class="form-group">
+                                <label for="password_confirmation">تکرار کلمه عبور</label>
+                                <input type="password" name="password_confirmation" class="form-control form-control-sm">
+                            </div>
+                            @error('password_confirmation')
+                                <span class="alert_required bg-danger text-white p-1 rounded" role="alert">
+                                    <strong>
+                                        {{$message}}
+                                    </strong>
+                                </span>
+                            @enderror
+                        </section>
+
+
+                        <section class="col-12 col-md-6 my-2">
+                            <div class="form-group">
+                                <label for="">تصویر</label>
+                                <input type="file" name="profile_photo_path" class="form-control form-control-sm">
+                            </div>
+                            @error('profile_photo_path')
+                                <span class="alert_required bg-danger text-white p-1 rounded" role="alert">
+                                    <strong>
+                                        {{$message}}
+                                    </strong>
+                                </span>
+                            @enderror
+                        </section>
+
+                        <section class="col-12 col-md-6 my-2">
+                            <div class="form-group">
+                                <label for=" activation">وضعیت فعال سازی کاربر</label>
+                                <select name=" activation" id=" activation" class="form-control form-control-sm">
+                                    <option value="0" @if (old(' activation')==0) selected @endif>غیرفعال</option>
+                                    <option value="1" @if (old(' activation')==1) selected @endif>فعال</option>
                                 </select>
                             </div>
+                            @error(' activation')
+                                <span class="alert_required bg-danger text-white p-1 rounded" role="alert">
+                                    <strong>
+                                        {{$message}}
+                                    </strong>
+                                </span>
+                            @enderror
                         </section>
+
                         <section class="col-12">
                             <button class="btn btn-primary btn-sm">ثبت</button>
                         </section>
--- resources/views/admin/user/admin-user/index.blade.php
+++ resources/views/admin/user/admin-user/index.blade.php
@@ -25,7 +25,7 @@
             </section>
 
             <section class="d-flex justify-content-between align-items-center mt-4 mb-3 border-bottom pb-2">
-                <a href="{{ route('admin.user.admin-user.create') }}" class="btn btn-info btn-sm">ایجاد ادمین جدید</a>
+                <a href="{{ route('admin.user.admin-user.create') }}" class="btn btn-info btn-sm">ایجاد کاربر ادمین جدید</a>
                 <div class="max-width-16-rem">
                     <input type="text" class="form-control form-control-sm form-text" placeholder="جستجو">
                 </div>
@@ -40,51 +40,46 @@
                             <th>شماره موبایل</th>
                             <th>نام</th>
                             <th>نام خانوادگی</th>
+                            <th>فعال سازی </th>
+                            <th>وضعیت</th>
                             <th>نقش</th>
                             <th class="max-width-16-rem text-center"><i class="fa fa-cogs"></i> تنظیمات</th>
                         </tr>
                     </thead>
                     <tbody>
+                        @foreach ($admins as $key => $admin)
                         <tr>
-                            <th>1</th>
-                            <td>kamran@gmail.com		</td>
-                            <td>09127468392	</td>
-                            <td>کامران	</td>
-                            <td>محمدی	</td>
+                            <th>{{$key + 1}}</th>
+                            <td>{{$admin->email}}</td>
+                            <td>{{$admin->mobile}}</td>
+                            <td>{{$admin->first_name}}</td>
+                            <td>{{$admin->last_name}}</td>
+                            <td>
+                                <label>
+                                    <input id="{{ $admin->id }}-active" onchange="changeActive({{ $admin->id }})" data-url="{{ route('admin.user.admin-user.activation', $admin->id) }}" type="checkbox" @if ($admin->activation === 1)
+                                    checked
+                                    @endif>
+                                </label>
+                            </td>
+                            <td>
+                                <label>
+                                    <input id="{{ $admin->id }}" onchange="changeStatus({{ $admin->id }})" data-url="{{ route('admin.user.admin-user.status', $admin->id) }}" type="checkbox" @if ($admin->status === 1)
+                                    checked
+                                    @endif>
+                                </label>
+                            </td>
                             <td>سوپر ادمین	</td>
                             <td class="width-22-rem text-left">
                                 <a href="#" class="btn btn-warning btn-sm"><i class="fa fa-edit"></i> نقش</a>
-                                <a href="#" class="btn btn-primary btn-sm"><i class="fa fa-edit"></i> ویرایش</a>
-                                <button class="btn btn-danger btn-sm" type="submit"><i class="fa fa-trash-alt"></i> حذف</button>
+                                <a href="{{ route('admin.user.admin-user.edit', $admin->id) }}" class="btn btn-primary btn-sm"><i class="fa fa-edit"></i> ویرایش</a>
+                                <form class="d-inline" action="{{ route('admin.user.admin-user.destroy', $admin->id) }}" method="POST">
+                                    @csrf
+                                    {{method_field('DELETE')}}
+                                    <button class="btn btn-danger btn-sm delete" type="submit"><i class="fa fa-trash-alt"></i> حذف</button>
+                                </form>
                             </td>
                         </tr>
-                        <tr>
-                            <th>1</th>
-                            <td>kamran@gmail.com		</td>
-                            <td>09127468392	</td>
-                            <td>کامران	</td>
-                            <td>محمدی	</td>
-                            <td>سوپر ادمین	</td>
-                            <td class="width-22-rem text-left">
-                                <a href="#" class="btn btn-warning btn-sm"><i class="fa fa-edit"></i> نقش</a>
-                                <a href="#" class="btn btn-primary btn-sm"><i class="fa fa-edit"></i> ویرایش</a>
-                                <button class="btn btn-danger btn-sm" type="submit"><i class="fa fa-trash-alt"></i> حذف</button>
-                            </td>
-                        </tr>
-                        <tr>
-                            <th>1</th>
-                            <td>kamran@gmail.com		</td>
-                            <td>09127468392	</td>
-                            <td>کامران	</td>
-                            <td>محمدی	</td>
-                            <td>سوپر ادمین	</td>
-                            <td class="width-22-rem text-left">
-                                <a href="#" class="btn btn-warning btn-sm"><i class="fa fa-edit"></i> نقش</a>
-                                <a href="#" class="btn btn-primary btn-sm"><i class="fa fa-edit"></i> ویرایش</a>
-                                <button class="btn btn-danger btn-sm" type="submit"><i class="fa fa-trash-alt"></i> حذف</button>
-                            </td>
-                        </tr>
-
+                        @endforeach
                     </tbody>
                 </table>
             </section>
@@ -94,3 +89,148 @@
 </section>
 
 @endsection
+
+@section('script')
+
+    <script type="text/javascript">
+        function changeStatus(id){
+            var element = $("#" + id)
+            var url = element.attr('data-url')
+            var elementValue = !element.prop('checked');
+
+            $.ajax({
+                url : url,
+                type : "GET",
+                success : function(response){
+                    if(response.status){
+                        if(response.checked){
+                            element.prop('checked', true);
+                            successToast('ادمین  با موفقیت فعال شد')
+                        }
+                        else{
+                            element.prop('checked', false);
+                            successToast('ادمین با موفقیت غیر فعال شد')
+                        }
+                    }
+                    else{
+                        element.prop('checked', elementValue);
+                        errorToast('هنگام ویرایش مشکلی بوجود امده است')
+                    }
+                },
+                error : function(){
+                    element.prop('checked', elementValue);
+                    errorToast('ارتباط برقرار نشد')
+                }
+            });
+
+            function successToast(message){
+
+                var successToastTag = '<section class="toast" data-delay="5000">\n' +
+                    '<section class="toast-body py-3 d-flex bg-success text-white">\n' +
+                        '<strong class="ml-auto">' + message + '</strong>\n' +
+                        '<button type="button" class="mr-2 close" data-dismiss="toast" aria-label="Close">\n' +
+                            '<span aria-hidden="true">&times;</span>\n' +
+                            '</button>\n' +
+                            '</section>\n' +
+                            '</section>';
+
+                            $('.toast-wrapper').append(successToastTag);
+                            $('.toast').toast('show').delay(5500).queue(function() {
+                                $(this).remove();
+                            })
+            }
+
+            function errorToast(message){
+
+                var errorToastTag = '<section class="toast" data-delay="5000">\n' +
+                    '<section class="toast-body py-3 d-flex bg-danger text-white">\n' +
+                        '<strong class="ml-auto">' + message + '</strong>\n' +
+                        '<button type="button" class="mr-2 close" data-dismiss="toast" aria-label="Close">\n' +
+                            '<span aria-hidden="true">&times;</span>\n' +
+                            '</button>\n' +
+                            '</section>\n' +
+                            '</section>';
+
+                            $('.toast-wrapper').append(errorToastTag);
+                            $('.toast').toast('show').delay(5500).queue(function() {
+                                $(this).remove();
+                            })
+            }
+        }
+    </script>
+
+
+    <script type="text/javascript">
+        function changeActive(id){
+            var element = $("#" + id + '-active')
+        var url = element.attr('data-url')
+        var elementValue = !element.prop('checked');
+
+            $.ajax({
+                url : url,
+                type : "GET",
+                success : function(response){
+                    if(response.status){
+                        if(response.checked){
+                            element.prop('checked', true);
+                            successToast('فعال سازی ادمین با موفقیت انجام شد')
+                        }
+                        else{
+                            element.prop('checked', false);
+                            successToast('غیر فعال سازی ادمین با موفقیت انجام شد')
+                        }
+                    }
+                    else{
+                        element.prop('checked', elementValue);
+                        errorToast('هنگام ویرایش مشکلی بوجود امده است')
+                    }
+                },
+                error : function(){
+                    element.prop('checked', elementValue);
+                    errorToast('ارتباط برقرار نشد')
+                }
+            });
+
+            function successToast(message){
+
+                var successToastTag = '<section class="toast" data-delay="5000">\n' +
+                    '<section class="toast-body py-3 d-flex bg-success text-white">\n' +
+                        '<strong class="ml-auto">' + message + '</strong>\n' +
+                        '<button type="button" class="mr-2 close" data-dismiss="toast" aria-label="Close">\n' +
+                            '<span aria-hidden="true">&times;</span>\n' +
+                            '</button>\n' +
+                            '</section>\n' +
+                            '</section>';
+
+                            $('.toast-wrapper').append(successToastTag);
+                            $('.toast').toast('show').delay(5500).queue(function() {
+                                $(this).remove();
+                            })
+            }
+
+            function errorToast(message){
+
+                var errorToastTag = '<section class="toast" data-delay="5000">\n' +
+                    '<section class="toast-body py-3 d-flex bg-danger text-white">\n' +
+                        '<strong class="ml-auto">' + message + '</strong>\n' +
+                        '<button type="button" class="mr-2 close" data-dismiss="toast" aria-label="Close">\n' +
+                            '<span aria-hidden="true">&times;</span>\n' +
+                            '</button>\n' +
+                            '</section>\n' +
+                            '</section>';
+
+                            $('.toast-wrapper').append(errorToastTag);
+                            $('.toast').toast('show').delay(5500).queue(function() {
+                                $(this).remove();
+                            })
+            }
+        }
+    </script>
+
+
+@include('admin.alerts.sweetalert.delete-confirm', ['className' => 'delete'])
+
+
+@endsection
+
+
