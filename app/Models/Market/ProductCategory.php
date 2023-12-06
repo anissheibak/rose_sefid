d117f2898a749d4ec4c030477d184b967bdb35fa
anissheibak <sheibakanis@gmail.com>
2023-12-06 19:56:49 +0330

    code updated.

--- app/Models/Market/ProductCategory.php
+++ app/Models/Market/ProductCategory.php
@@ -37,7 +37,7 @@ class ProductCategory extends Model
 
     public function products():HasMany
     {
-        return $this->hasMany(Product::class);
+        return $this->hasMany(Product::class, 'category_id');
     }
 
     public function attributes():HasMany
