a63e0d0dd6b6b9a33e73bc5fcc2632b272e94573
anissheibak <sheibakanis@gmail.com>
2023-12-09 23:40:55 +0330

    Product Controller updated.

--- app/Http/Controllers/Admin/Market/ProductController.php
+++ app/Http/Controllers/Admin/Market/ProductController.php
@@ -139,22 +139,25 @@ class ProductController extends Controller
 
         $product->update($inputs);
 
-        $meta_keys = $request->meta_key;
-        $meta_values = $request->meta_value;
-        $meta_ids = array_keys($request->meta_key);
-        $metas = array_map(function ($meta_id, $meta_key, $meta_value){
-            return array_combine(
-                ['meta_id', 'meta_key', 'meta_value'],
-                [$meta_id, $meta_key, $meta_value]
-            );
-        }, $meta_ids, $meta_keys, $meta_values);
+        if($request->meta_key != null){
+            $meta_keys = $request->meta_key;
+            $meta_values = $request->meta_value;
+            $meta_ids = array_keys($request->meta_key);
+            $metas = array_map(function ($meta_id, $meta_key, $meta_value){
+                return array_combine(
+                    ['meta_id', 'meta_key', 'meta_value'],
+                    [$meta_id, $meta_key, $meta_value]
+                );
+            }, $meta_ids, $meta_keys, $meta_values);
 
-        foreach($metas as $meta){
-            ProductMeta::where('id', $meta['meta_id'])->update([
-                'meta_key' => $meta['meta_key'],
-                'meta_value' => $meta['meta_value']
-                ]);
+            foreach($metas as $meta){
+                ProductMeta::where('id', $meta['meta_id'])->update([
+                    'meta_key' => $meta['meta_key'],
+                    'meta_value' => $meta['meta_value']
+                    ]);
+            }
         }
+
 
         return redirect()->route('admin.market.product.index')->with('swal-success', 'محصول با موفقیت ویرایش شد.');
     }
