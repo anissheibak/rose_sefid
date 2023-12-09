7107b4981dbc3143b1e598eca495f0d9656e83b1
anissheibak <sheibakanis@gmail.com>
2023-12-09 22:25:05 +0330

    product comment completed.

--- app/Http/Controllers/Admin/Market/CommentController.php
+++ app/Http/Controllers/Admin/Market/CommentController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Admin\Market;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\Content\CommentRequest;
+use App\Models\Content\Comment;
 use Illuminate\Http\Request;
 
 class CommentController extends Controller
@@ -14,7 +16,8 @@ class CommentController extends Controller
      */
     public function index()
     {
-        return view('admin.market.comment.index');
+        $comments = Comment::orderBy('created_at', 'desc')->where('commentable_type', 'App\Models\Market\Product')->simplePaginate(15);
+        return view('admin.market.comment.index', compact('comments'));
     }
 
     /**
@@ -44,9 +47,11 @@ class CommentController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show()
+    public function show(Comment $comment)
     {
-        return view('admin.market.comment.show');
+        $comment->seen = 1;
+        $comment->save();
+        return view('admin.market.comment.show', compact('comment'));
     }
 
     /**
@@ -81,5 +86,56 @@ class CommentController extends Controller
     public function destroy($id)
     {
         //
+    }
+
+    public function status(Comment $comment){
+
+        $comment->status = $comment->status == 0 ? 1 : 0;
+        $result = $comment->save();
+        if($result){
+                if($comment->status == 0){
+                    return response()->json(['status' => true, 'checked' => false]);
+                }
+                else{
+                    return response()->json(['status' => true, 'checked' => true]);
+                }
+        }
+        else{
+            return response()->json(['status' => false]);
+        }
+
+    }
+
+    public function approved(Comment $comment){
+
+        $comment->approved = $comment->approved == 0 ? 1 : 0;
+        $result = $comment->save();
+        if($result){
+            return redirect()->route('admin.market.comment.index')->with('swal-success', '  وضعیت نظر با موفقیت تغییر کرد');
+        }
+        else{
+            return redirect()->route('admin.market.comment.index')->with('swal-error', '  وضعیت نظر با خطا مواجه شد');
+        }
+
+    }
+
+
+    public function answer(CommentRequest $request, Comment $comment)
+    {
+        if ($comment->parent == null) {
+            $inputs = $request->all();
+            $inputs['author_id'] = 1;
+            $inputs['parent_id'] = $comment->id;
+            $inputs['commentable_id'] = $comment->commentable_id;
+            $inputs['commentable_type'] = $comment->commentable_type;
+            $inputs['approved'] = 1;
+            $inputs['status'] = 1;
+            $comment = Comment::create($inputs);
+            return redirect()->route('admin.market.comment.index')->with('swal-success', '  پاسخ شما با موفقیت ثبت شد');
+        }
+        else{
+            return redirect()->route('admin.market.comment.index')->with('swal-error', 'خطا');
+
+        }
     }
 }
--- resources/views/admin/market/comment/show.blade.php
+++ resources/views/admin/market/comment/show.blade.php
@@ -31,30 +31,39 @@
 
             <section class="card mb-3">
                 <section class="card-header text-white bg-custom-yellow">
-                    کامران محمدی - 845362736
+                    {{ $comment->user->fullName  }} - {{ $comment->user->id  }}
                 </section>
                 <section class="card-body">
-                    <h5 class="card-title">مشخصات کالا : ساعت هوشمند apple watch کد کالا : 8974938</h5>
-                    <p class="card-text">به نظر من ساعت خوبیه ولی تنها مشکلی که داره اینه که وزنش زیاده و زود شارژش تموم میشه!</p>
+                    <h5 class="card-title">مشخصات کالا : {{ $comment->commentable->title }} کد کالا : {{ $comment->commentable->id }}</h5>
+                    <p class="card-text">{{ $comment->body }}</p>
                 </section>
             </section>
 
+            @if($comment->parent_id == null)
             <section>
-                <form action="" method="">
+                <form action="{{ route('admin.market.comment.answer', $comment->id) }}" method="post">
+                    @csrf
                     <section class="row">
                         <section class="col-12">
                             <div class="form-group">
                                 <label for="">پاسخ ادمین</label>
-                               ‍<textarea class="form-control form-control-sm" rows="4"></textarea>
+                               ‍<textarea class="form-control form-control-sm" name="body" rows="4"></textarea>
                             </div>
+                            @error('body')
+                            <span class="alert_required bg-danger text-white p-1 rounded" role="alert">
+                                <strong>
+                                    {{ $message }}
+                                </strong>
+                            </span>
+                        @enderror
                         </section>
                         <section class="col-12">
-                            <button class="btn btn-primary btn-sm">ثبت</button>
+                            <button class="btn btn-primary btn-sm my-2">ثبت</button>
                         </section>
                     </section>
                 </form>
             </section>
-
+            @endif
         </section>
     </section>
 </section>
