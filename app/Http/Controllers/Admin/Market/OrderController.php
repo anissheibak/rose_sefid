0cbfec75c9be65fcb5a259b6e6a857c5329a5635
anissheibak <sheibakanis@gmail.com>
2024-03-15 15:37:08 +0330

    order controller updated.

--- app/Http/Controllers/Admin/Market/OrderController.php
+++ app/Http/Controllers/Admin/Market/OrderController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin\Market;
 
 use App\Http\Controllers\Controller;
+use App\Models\Market\Order;
 use Illuminate\Http\Request;
 
 class OrderController extends Controller
@@ -29,7 +30,8 @@ class OrderController extends Controller
     }
     public function all()
     {
-        return view('admin.market.order.index');
+        $orders = Order::all();
+        return view('admin.market.order.index', compact('orders'));
     }
     public function show()
     {
