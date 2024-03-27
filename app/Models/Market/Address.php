e0f17322340f79ed6c15c6d5d720e630abaa7850
anissheibak <sheibakanis@gmail.com>
2024-03-27 22:35:36 +0330

    Address model created and completed.

---
+++ app/Models/Market/Address.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models\Market;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Address extends Model
+{
+    use HasFactory;
+
+    public function city():BelongsTo
+    {
+        return $this->belongsTo(City::class);
+    }
+}
