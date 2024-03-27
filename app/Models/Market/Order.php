1a1f5aa3498a0972361a02888599e77a2388c48a
anissheibak <sheibakanis@gmail.com>
2024-03-27 22:33:58 +0330

    order model updated.

--- app/Models/Market/Order.php
+++ app/Models/Market/Order.php
@@ -2,9 +2,11 @@
 
 namespace App\Models\Market;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Order extends Model
@@ -19,5 +21,30 @@ class Order extends Model
     public function delivery(): BelongsTo
     {
         return $this->belongsTo(Delivery::class);
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function address(): BelongsTo
+    {
+        return $this->belongsTo(Address::class);
+    }
+
+    public function copan(): BelongsTo
+    {
+        return $this->belongsTo(Copan::class);
+    }
+
+    public function commonDiscount(): BelongsTo
+    {
+        return $this->belongsTo(CommonDiscount::class);
+    }
+
+    public function orderItems(): HasMany
+    {
+        return $this->hasMany(OrderItem::class);
     }
 }
