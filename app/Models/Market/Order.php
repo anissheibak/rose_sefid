bcc8ff75bd52762c89ce4cc7ef4f3a7995b5e0ea
anissheibak <sheibakanis@gmail.com>
2024-04-02 23:20:31 +0330

    Add accessors for delivery status, payment status and type, order status.

--- app/Models/Market/Order.php
+++ app/Models/Market/Order.php
@@ -47,4 +47,88 @@ class Order extends Model
     {
         return $this->hasMany(OrderItem::class);
     }
+
+    public function getPaymentStatusValueAttribute()
+    {
+        switch ($this->payment_status)
+        {
+            case 0:
+                $result = 'پرداخت نشده';
+                break;
+            case 1:
+                $result = 'پرداخت شده';
+                break;
+            case 2:
+                $result = 'باطل شده';
+                break;
+            default:
+                $result = 'برگشت داده شده';
+                break;
+        }
+        return $result;
+    }
+
+    public function getPaymentTypeValueAttribute()
+    {
+        switch ($this->payment_type)
+        {
+            case 0:
+                $result = 'آنلاین';
+                break;
+            case 1:
+                $result = 'آفلاین';
+                break;
+            default:
+                $result = 'در محل';
+                break;
+        }
+        return $result;
+    }
+
+    public function getDeliveryStatusValueAttribute()
+    {
+        switch ($this->delivery_status)
+        {
+            case 0:
+                $result = 'ارسال نشده';
+                break;
+            case 1:
+                $result = 'در حال ارسال';
+                break;
+            case 2:
+                $result = 'ارسال شده';
+                break;
+            default:
+                $result = 'تحویل شده';
+                break;
+        }
+        return $result;
+    }
+
+    public function getOrderStatusValueAttribute()
+    {
+        switch ($this->order_status)
+        {
+            case 1:
+                $result = 'در انتظار تایید';
+                break;
+            case 2:
+                $result = 'تایید نشده';
+                break;
+            case 3:
+                $result = 'تایید شده';
+                break;
+            case 4:
+                $result = 'باطل شده';
+                break;
+            case 5:
+                $result = 'مرجوع شده';
+                break;
+            default:
+                $result = 'بررسی نشده';
+                break;
+        }
+        return $result;
+    }
+
 }
