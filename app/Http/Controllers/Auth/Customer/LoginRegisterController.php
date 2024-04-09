729030a5007e7e15c1f05076da58e340d71a6e9b
anissheibak <sheibakanis@gmail.com>
2024-04-09 23:21:58 +0330

    email service implemented.

--- app/Http/Controllers/Auth/Customer/LoginRegisterController.php
+++ app/Http/Controllers/Auth/Customer/LoginRegisterController.php
@@ -11,6 +11,7 @@ use Illuminate\Support\Facades\Config;
 use App\Http\Services\Message\MessageService;
 use App\Http\Services\Message\SMS\SmsService;
 use App\Http\Requests\Auth\Customer\LoginRegisterRequest;
+use App\Http\Services\Message\Email\EmailService;
 
 class LoginRegisterController extends Controller
 {
@@ -75,7 +76,8 @@ class LoginRegisterController extends Controller
 
         //send sms or email
 
-        if($type == 0){
+        if($type == 0)
+        {
             //send sms
             $smsService = new SmsService();
             $smsService->setFrom(Config::get('sms.otp_from'));
@@ -87,8 +89,22 @@ class LoginRegisterController extends Controller
 
         }
 
+        elseif($type == 1)
+        {
+            $emailService = new EmailService;
+            $details = [
+                'title' => 'ایمیل فعال‌سازی',
+                'body' => "کد فعال‌سازی شما: $otpCode"
+            ];
+            $emailService->setDetails($details);
+            $emailService->setFrom('noreply@example.com', 'example');
+            $emailService->setTo($inputs['id']);
+
+            $messagesService = new MessageService($emailService);
+        }
+
         $messagesService->send();
-        dd('soidvodv');
+
 
     }
 }
---
+++ resources/views/mails/layouts/footer.blade.php
@@ -0,0 +1,20 @@
+
+<!-- start footer -->
+<footer class="footer">
+    <section class="container-xxl my-4">
+        <section class="row my-5">
+            <section class="col">
+                <section class="fw-bold">گلسرای رز سفید</section>
+                <section class="text-muted footer-intro">ما همواره تلاش می کنیم بهترین خدمات را به مشتریان رز سفید ارائه کنیم. به شما کمک می کنیم بهترین انتخاب را داشته باشید و با اطمینان خاطر خرید را انجام بدهید و در کوتاه ترین زمان ممکن محصول خود را دریافت کنید. همچنین ما  در هفت روز هفته به مشتریان مان خدمات ارائه می دهیم. .</section>
+            </section>
+        </section>
+
+        <section class="row border-top pt-4">
+            <section class="col">
+                <section class="text-muted footer-intro text-center">کلیه حقوق این وبسایت متعلق به گلسرای رز سفید می باشد.</section>
+            </section>
+        </section>
+    </section>
+</footer>
+<!-- end footer -->
+
