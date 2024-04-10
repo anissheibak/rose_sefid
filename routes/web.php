e34aae4abe8d31c54be05d7d3ae34aace9e6a238
anissheibak <sheibakanis@gmail.com>
2024-04-10 23:59:29 +0330

    login confirm and resend code routes added.

--- routes/web.php
+++ routes/web.php
@@ -434,6 +434,9 @@ Route::prefix('admin')->namespace('Admin')->group(function () {
 Route::namespace('Auth')->group(function () {
     Route::get('login-register', [LoginRegisterController::class, 'loginRegisterForm'])->name('auth.customer.login-register-form');
     Route::post('/login-register', [LoginRegisterController::class, 'loginRegister'])->name('auth.customer.login-register');
+    Route::get('login-confirm/{token}', [LoginRegisterController::class, 'loginConfirmForm'])->name('auth.customer.login-confirm-form');
+    Route::post('/login-confirm/{token}', [LoginRegisterController::class, 'loginConfirm'])->name('auth.customer.login-confirm');
+    Route::get('/login-resend-otp/{token}', [LoginRegisterController::class, 'loginResendOtp'])->name('auth.customer.login-resend-otp');
 });
 
 
