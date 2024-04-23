5a310798b82ae6e8ff4a221acd47911478abef0c
anissheibak <sheibakanis@gmail.com>
2024-04-23 19:02:28 +0330

    slideshow created.

--- resources/views/customer/home.blade.php
+++ resources/views/customer/home.blade.php
@@ -7,18 +7,20 @@
         <section class="row">
             <section class="col-md-8 pe-md-1 ">
                 <section id="slideshow" class="owl-carousel owl-theme">
-                    <section class="item"><a class="w-100 d-block h-auto text-decoration-none" href="#"><img class="w-100 rounded-2 d-block h-auto" src="assets/images/slideshow/1.jpg" alt=""></a></section>
-                    <section class="item"><a class="w-100 d-block h-auto text-decoration-none" href="#"><img class="w-100 rounded-2 d-block h-auto" src="assets/images/slideshow/2.jpg" alt=""></a></section>
-                    <section class="item"><a class="w-100 d-block h-auto text-decoration-none" href="#"><img class="w-100 rounded-2 d-block h-auto" src="assets/images/slideshow/3.jpg" alt=""></a></section>
-                    <section class="item"><a class="w-100 d-block h-auto text-decoration-none" href="#"><img class="w-100 rounded-2 d-block h-auto" src="assets/images/slideshow/4.jpg" alt=""></a></section>
-                    <section class="item"><a class="w-100 d-block h-auto text-decoration-none" href="#"><img class="w-100 rounded-2 d-block h-auto" src="assets/images/slideshow/5.jpg" alt=""></a></section>
-                    <section class="item"><a class="w-100 d-block h-auto text-decoration-none" href="#"><img class="w-100 rounded-2 d-block h-auto" src="assets/images/slideshow/6.gif" alt=""></a></section>
+
+                    @foreach ($slideShowImages as $slideShowImage)
+                        <section class="item">
+                            <a class="w-100 d-block h-auto text-decoration-none" href="{{urldecode($slideShowImage->url)}}">
+                                <img class="w-100 rounded-2 d-block h-auto" src="{{ asset($slideShowImage->image) }}" alt="">
+                            </a>
+                        </section>
+                    @endforeach
+
                 </section>
             </section>
             <section class="col-md-4 ps-md-1 mt-2 mt-md-0">
                 <section class="mb-2"><a href="#" class="d-block"><img class="w-100 rounded-2" src="assets/images/slideshow/12.gif" alt=""></a></section>
-                <section class="mb-2"><a href="#" class="d-block"><img class="w-100 rounded-2" src="assets/images/slideshow/11.jpg" alt=""></a></section>
-            </section>
+
         </section>
     </section>
     <!-- end slideshow -->
